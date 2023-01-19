437687e4f7e2b50b73eda177890ff07aa8c7b50e
trteel1 <tateelamino@gmail.com>
2023-01-19 19:26:47 +0300

    Completed assignment

--- demo.js
+++ demo.js
@@ -1,20 +1,17 @@
 // We use the Objects below to control toggling like / unlike status
-
 const glyphStates = {
   "♡": "♥",
   "♥": "♡"
 };
-
 const colorStates = {
   "red" : "",
   "": "red"
 };
-
 // STEP 1: The line of code below is what lets JavaScript find the elements that
 // we want to make clickable. Without JavaScript, clicking on these heart shapes
 // does nothing. Uncomment the code and refresh the demo page. 
 
-// const articleHearts = document.querySelectorAll(".like-glyph");
+const articleHearts = document.querySelectorAll(".like-glyph");
 
 function likeCallback(e) {
   const heart = e.target;
@@ -25,22 +22,23 @@ function likeCallback(e) {
       // mimicking Pillar 3 (Server Communication) to only update the screen if
       // the sending of information to the server succeeds.
       alert("You notified the server!");
-      // alert(serverMessage);
-      // heart.innerText = glyphStates[heart.innerText];
-      // heart.style.color = colorStates[heart.style.color];
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
     })
     .catch(function(error) {
       alert("Something went wrong!");
     });
+
 }
 
 // STEP 3: In order for the call to the server and the update of the screen to
 // work, we need to add a click event listener to the elements we identified in
 // STEP 1. That's Pillar 2, event handling. Uncomment this code:
 
-// for (const glyph of articleHearts) {
-//   glyph.addEventListener("click", likeCallback);
-// }
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 // STEP 4: 
 
@@ -48,11 +46,9 @@ function likeCallback(e) {
 // working demonstration of our reference example. Sure, it's maybe not as
 // pretty as a professional site, but they're only different from our site in
 // style, not substance.
-
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
-
 function mimicServerCall() {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
